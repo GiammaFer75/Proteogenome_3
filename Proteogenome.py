840917649c2aff0d92fc1296fc25cf2ccee1b189
GiammaFer75 <giammafer@yahoo.com>
2023-06-30 16:49:21 +0100

    Flags + input data
    Added the:
    . flags management
     - flags definition
     - variables to store flags input from the command line
     - control for double annotations input
    
    . indexes generation
     - call load_input_table function to upload the peptide table
     - call initialise_indexes function

--- Proteogenome.py
+++ Proteogenome.py
@@ -1,4 +1,8 @@
 #!usr/bin/python3
+
+from Proteogenome3 import pg_indexes as pg_i
+from Proteogenome3 import pg_input
+
 from absl import app
 from absl import flags
 
@@ -18,7 +22,8 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string('pogo_windows_exe', None, 'The executable PoGo file for running the software on Windows OS.')
 flags.DEFINE_string('protein_FASTA_seq', None, 'The protein sequences related to the reference organism genome.')
-flags.DEFINE_string('protein_GTF_annots', None, 'The protein annotations related to the reference organism genome.')
+flags.DEFINE_string('protein_GFF3_annots', None, 'The protein annotations related to the reference organism genome in GFF3.')
+flags.DEFINE_string('protein_GTF_annots', None, 'The protein annotations related to the reference organism genome in GTF.')
 flags.DEFINE_string('peptides_table', None, 'The table containing the peptides sequences with the format:\n'
                                             '- Protein ID\n'
                                             '- Peptide sequence\n'
@@ -31,19 +36,34 @@ def main(argv):
     # ***** FLAGS DEFINITION ***** #
     pogo_windows_exe_path = FLAGS.pogo_windows_exe
     protein_FASTA_seq_path = FLAGS.protein_FASTA_seq
+    protein_GFF3_annots_path = FLAGS.protein_GFF3_annots
     protein_GTF_annots_path = FLAGS.protein_GTF_annots
     peptides_table_path = FLAGS.peptides_table
 
-    # Upload files and create indexes
+    print(protein_GFF3_annots_path)
+    print(protein_GTF_annots_path)
+    # Set the annotation format flag
+    if (protein_GFF3_annots_path != None) and (protein_GTF_annots_path == None): annotations_format = 'gff3'
+    elif (protein_GTF_annots_path != None) and (protein_GFF3_annots_path == None): annotations_format = 'gtf'
+    else: raise Exception("Apparently have been provided two genome annotations. Please input one annotation only.")
 
+    # Upload files and create indexes
+    peptides_input_table = pg_input.load_input_table(peptides_table_path)
+
+
+# ********************* REMAINING ROWS FROM PROTEOGENOME2
+    prot_CDS_index = {}  # Initialise dictionary for protein ---> CDS index
+    prot_PSMint_index = {}  # Initialise dictionary for protein ---> PSM - intensity - RGB intensity index
+# *********************
+
+
+    CDS_matrix, prot_CDS_index, protein_pep_index, pep_protein_index = pg_i.initialise_indexes(protein_GTF_annots_path, peptides_input_table, annot_format=annotations_format)
 
     #TESTING COMMANDS
     pi.print_input(pogo_windows_exe_path, protein_FASTA_seq_path, protein_GTF_annots_path, peptides_table_path)
     chdir(pogo_dir)
     subprocess.run(['.\PoGo.exe', 'capture_output=True'])
     subprocess.run(['dir'], shell=True)
-
-
 
 
 if __name__ == '__main__':
