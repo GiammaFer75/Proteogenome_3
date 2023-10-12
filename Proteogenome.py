13fbc1ac8c5a810ea39a94354020abbc47c77a32
GiammaFer75 <giammafer@yahoo.com>
2023-10-12 17:36:31 +0100

    Added UniProt to Ensamble conversion management
    The software collect the UniProt accession codes and convert in Ensembl codes.
    
    If this conversion table does not exist at all the new one is created.
    If the table exists the software compare the UniProt codes already converted, comparing them with the codes provided in input. If some of them are missed in the conversion table, then it performs the conversion and updates the original conversion table with the new entries.

--- Proteogenome.py
+++ Proteogenome.py
@@ -16,6 +16,7 @@ from os import chdir, makedirs
 
 import shutil
 import pathlib
+import json
 
 from Test_PoGo_absl_subprocess import printinput as pi
 
@@ -23,10 +24,13 @@ from Test_PoGo_absl_subprocess import printinput as pi
 #                               PATHS DEFINITION                                 #
 home_dir = 'C:/Bioinformatics/Proteogenome_v3/Test_PoGo_absl_subprocess/'
 pogo_dir = 'C:/Bioinformatics/Proteogenome_v3/Test_PoGo_absl_subprocess/Proteogenome-master/Proteogenome-master/PoGo/Windows'
+install_folder = 'C:/Bioinformatics/Proteogenome_v3/Proteogenome_Install'
 # ****************************************************************************** #
 
 
 FLAGS = flags.FLAGS
+
+flags.DEFINE_string('install_folder', None, 'The installation folder')
 
 flags.DEFINE_string('species', None, 'The species to map')
 
@@ -54,14 +58,17 @@ def main(argv):
     else: project_dir_path = pathlib.Path.cwd()
 
     # INPUT WITH JSON FILE
-
     if pg_utils.check_input_json(project_dir_path):
+        pg_install_folder_path = pathlib.Path(install_folder) # Now the installation folder is static
         print('Input JSON file detected')
         pogo_windows_exe_path, protein_FASTA_seq_path, protein_GFF3_annots_path, protein_GTF_annots_path, \
         peptides_table_path, species = pg_utils.input_json(project_dir_path)
-        print('PATH - ',pogo_windows_exe_path, protein_FASTA_seq_path, protein_GFF3_annots_path, protein_GTF_annots_path, peptides_table_path, species )
+        #print('PATH - ',pogo_windows_exe_path, protein_FASTA_seq_path, protein_GFF3_annots_path, protein_GTF_annots_path, peptides_table_path, species )
     else:
-        # INPUT WITH FLAGS
+    # INPUT WITH FLAGS
+        if not FLAGS.install_folder:
+            pg_install_folder_path = pathlib.Path(install_folder) # Now the installation folder is static
+
         pogo_windows_exe_path = pathlib.Path(FLAGS.pogo_windows_exe)            # PoGo Executable
         protein_FASTA_seq_path = pathlib.Path(FLAGS.protein_FASTA_seq)          # FASTA protein sequences
         if FLAGS.protein_GFF3_annots:
@@ -73,6 +80,8 @@ def main(argv):
         peptides_table_path = pathlib.Path(FLAGS.peptides_table)                # Peptides Input Table
 
         species = FLAGS.species
+
+    print(f'Install folder - {pg_install_folder_path}')
 
     print(project_dir_path)
     print(pogo_windows_exe_path)
@@ -99,15 +108,43 @@ def main(argv):
     FASTA_seq_lst = pg_input.load_protein_FASTA_seq(protein_FASTA_seq_path)     # Upload protein FASTA sequences
     annot_lst = pg_input.file_to_lst(protein_GTF_annots_path)                   # Upload reference genome annotations
 
+    uniprot_to_ensembl_acc=[]
     # CONVERT UniProt accession in Ensemble accession in protein_pep_index
     if species == 'homo sapiens':
-        uniprot_to_ensembl_acc = pg_utils.UniProt2Ensembl(accession_lst=peptides_input_table[:,0],
-                                                          speces='homo sapiens')  # Dictionary of UniProt -> Ensemble acc
+        input_protein_list = peptides_input_table[:,0]
+        # Conversion table DO NOT EXISTS
+        if not pg_utils.check_input_json(pg_install_folder_path, proteogenome_input_json = 'uniprot_to_ensembl_acc.json'):
+            print('Protein conversion JSON file - NOT FOUND\nSTART the conversion of UniProt accession codes to Ensembl accession codes\n')
+            uniprot_to_ensembl_acc = pg_utils.UniProt2Ensembl(accession_lst=input_protein_list,
+                                                              speces='homo sapiens')  # Dictionary of UniProt -> Ensemble acc
+            # Save the conversion table from uniprot to Ensembl in a JSON file
+            f = open(pg_install_folder_path.joinpath('uniprot_to_ensembl_acc.json'), 'w')
+            json.dump(uniprot_to_ensembl_acc, f)
+            f.close()
+        # Conversion Table EXIST
+        else:
+            print(f'Protein conversion JSON file - FOUND\nUpload converted codes')
+            f = open(pg_install_folder_path.joinpath('uniprot_to_ensembl_acc.json'), 'r')
+            uniprot_to_ensembl_acc = json.load(f)
+            uniprot_to_ensembl_acc_keys = uniprot_to_ensembl_acc.keys()  # Extract UniProt codes from the conversion table for comparison
+            print(f'{len(uniprot_to_ensembl_acc_keys)} already converted\n')
+            f.close()
+            uniprot_not_matched = [uni_pro_acc for uni_pro_acc in input_protein_list if uni_pro_acc not in uniprot_to_ensembl_acc_keys]
+            if uniprot_not_matched:
+                print(f'Detected new UniProt accession codes not yet converted\n-----------------------\n{uniprot_not_matched}\nSTART conversion of unmatched codes')
+                uniprot_not_matched_converted = pg_utils.UniProt2Ensembl(accession_lst=uniprot_not_matched,
+                                                                         speces='homo sapiens')  # Dictionary of new UniProt -> Ensemble acc
+                uniprot_to_ensembl_acc.update(uniprot_not_matched_converted)
+                print('Update conversion table')
+                pg_output.save_json_file(pg_install_folder_path.joinpath('uniprot_to_ensembl_acc.json'), uniprot_to_ensembl_acc)
+
+
         # Update the UniProt accession with Ensemble accession (in GENCODE they use sequences reference to Ensembl)
         for uniprot_acc, ensembl_acc in uniprot_to_ensembl_acc.items():
             if ensembl_acc: peptides_input_table[peptides_input_table==uniprot_acc] = ensembl_acc
-        print(f'\npeptides_input_table converted in Ensembl\n')
+        print(f'\npeptides_input_table converted in Ensembl\n--------------------')
         for i in peptides_input_table: print(i)
+
     elif species == 'virus':
         # FASTA rectification
         FASTA_seq_lst = dc.rectify_rows(FASTA_seq_lst, target_sub_str=[('lcl|NC_006273.2_prot_', '')])
@@ -140,20 +177,25 @@ def main(argv):
     pg_i.initialise_indexes(protein_GTF_annots_path, peptides_input_table, annot_format=annotations_format)
 
     # SAVE INDEXES
-    #pg_output.make_sep_file(pg_data_structure_path.joinpath('CDS_matrix.txt'), CDS_matrix, sep='')
+    print('Save Data Structures')
+    # pg_output.make_sep_file(pg_data_structure_path.joinpath('CDS_matrix.txt'), CDS_matrix, sep='')
     pg_output.save_dict_list(prot_CDS_index, pg_data_structure_path.joinpath('prot_CDS_index.txt'), 'd')
     pg_output.save_dict_list(protein_pep_index, pg_data_structure_path.joinpath('protein_pep_index.txt'), 'd')
     pg_output.save_dict_list(pep_protein_index, pg_data_structure_path.joinpath('pep_protein_index.txt'), 'd')
+    print('Data Structure SAVED\n')
 
     # TESTING COMMANDS
     pi.print_input(pogo_windows_exe_path, protein_FASTA_seq_path, protein_GTF_annots_path, PoGo_input_table_path)
     chdir(pogo_windows_exe_path)              # Go in the PoGo executable folder
 
-    PoGo_command =[pathlib.Path(pogo_windows_exe_path, 'PoGo.exe'),
+    PoGo_command =[pathlib.Path(pogo_windows_exe_path, 'PoGo_windows_v1.0.0.exe'),
                    '-fasta', protein_FASTA_seq_path,
                    '-gtf', protein_GTF_annots_path,
                    '-in', PoGo_input_table_path]
-    print(f'\n################\n{PoGo_command}\n################\n')
+    print('################'.center(shutil.get_terminal_size().columns))
+    print('RUN PoGo'.center(shutil.get_terminal_size().columns))
+    print('################'.center(shutil.get_terminal_size().columns))
+    print(f'PoGo Command --- \n{PoGo_command}')
     # PoGo_command = '.\PoGo.exe'
     subprocess.run(PoGo_command, capture_output=True)
     pg_utils.move_files(PoGo_input_path, PoGo_output_path,filenames_patterns=['*.bed','*.gct',
@@ -167,7 +209,8 @@ def main(argv):
 
     # Proteins MAP
     proteogenome_peptide_MAP_path = pg_output_path.joinpath('Proteins_MAP.bed')
-    proteins_not_found = pg_output.gen_protein_track(protein_pep_index, prot_CDS_index, bed_fn=proteogenome_peptide_MAP_path)
+    proteins_not_found = pg_output.gen_protein_track(protein_pep_index, prot_CDS_index,
+                                                     strand='GRCh38', bed_fn=proteogenome_peptide_MAP_path)
     print(f'^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\nPROTEINS NOT FOUND\n{proteins_not_found}\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
 
     # Peptides MAP
@@ -179,10 +222,11 @@ def main(argv):
 
     # PTMs MAP
     PoGo_PTM_map_path = PoGo_output_path.joinpath('PoGo_Input_Table_ptm.bed')
-    PoGo_PTM_map_table = pg_input.load_generic_table(PoGo_PTM_map_path)
-    proteogenome_PTM_MAP_path = pg_output_path.joinpath('PTMs_MAP.bed')
-    print(f'Path for the PTM MAP ---- > {PoGo_PTM_map_path}')
-    dp.filter_peptides(PoGo_PTM_map_table, pep_protein_index, prot_CDS_index, out_file_name=proteogenome_PTM_MAP_path)
+    if PoGo_PTM_map_path.stat().st_size != 0:                     # Check if there is a PTM map
+        PoGo_PTM_map_table = pg_input.load_generic_table(PoGo_PTM_map_path)
+        proteogenome_PTM_MAP_path = pg_output_path.joinpath('PTMs_MAP.bed')
+        print(f'Path for the PTM MAP ---- > {PoGo_PTM_map_path}')
+        dp.filter_peptides(PoGo_PTM_map_table, pep_protein_index, prot_CDS_index, out_file_name=proteogenome_PTM_MAP_path)
 
 
 
