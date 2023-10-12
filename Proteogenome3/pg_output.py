616f34777a99481c5769254bcd0a2016a6931028
GiammaFer75 <giammafer@yahoo.com>
2023-10-12 18:13:06 +0100

    Added save_json_file - bed file format management for the human genomne
    Generating the protein map in bed format I found some errors (not yet fixed) using the human genome. So I split the function in two, with different procedure for human and viral genomes.

--- Proteogenome3/pg_output.py
+++ Proteogenome3/pg_output.py
@@ -1,5 +1,7 @@
 from Proteogenome3 import pg_indexes as pg_i
 
+import json
+
 
 def make_sep_file(out_file_name, input_array, sep=''):
     """
@@ -26,6 +28,8 @@ def make_sep_file(out_file_name, input_array, sep=''):
     """
 
     out_file_hand = open(out_file_name, 'w')
+
+    print(f'Saving procedure of variable type - {type(input_array)}')
 
     if type(input_array) == list and type(input_array[0]) == list:  # LIST of LISTS
         number_of_columns = len(input_array[0])
@@ -74,8 +78,12 @@ def save_dict_list(input_data, out_file_name, input_data_type='d'):
             out_file_hand.write(f'{row}\n')
     out_file_hand.close()
 
-
-def gen_protein_track(prot_pep_index, prot_CDS_index, prot_list=[], strand='NC_006273.2', bed_fn='test1.bed'):
+def save_json_file(input_file_path, json_object):
+    # Save the conversion table from uniprot to Ensembl in a JSON file
+    f = open(input_file_path, 'w')
+    json.dump(json_object, f)
+    f.close()
+def gen_protein_track(prot_pep_index, prot_CDS_index, prot_list=[], strand='', bed_fn='test1.bed'): # strand for HCMV NC_006273.2
     """
     Version: 1.0
     Name History: protein_set_bed - protein_track (from Proteogenome 3) - gen_protein_track
@@ -102,11 +110,25 @@ def gen_protein_track(prot_pep_index, prot_CDS_index, prot_list=[], strand='NC_0
     print(f'Start processing {len(prot_list)} proteins')
     prot_track_fh = open(bed_fn, 'w')
 
-    chr_name = ''
-    if strand == 'NC_006273.2':
-        chr_name = 'NC_006273.2'  # Find the chromosome name
-    else:
-        print('Option under construction. Check how to manage chromosome name in Human annotations\FASTA')
+    strand_position = 0
+    start_p, start_n, end_p, end_n = 0, 0, 0, 0
+    organism = ''
+
+    # Define the array parser based on the organism
+    if 'NC_006273' in strand:                              ## HCMV herpes virus 5 ##
+        organism = 'virus'
+        chr_name = 'NC_006273.2'  # The chromosome name (only one in viruses) is always the same in the HCMV
+        start_p, end_p = 0, 1
+        start_n, end_n = 1, 0
+        strand_position = 2
+
+    elif 'GRCh38' in strand:                                ## HOMO SAPIENS GRCh38 ##
+        # print('Option under construction. Check how to manage chromosome name in Human annotations\FASTA')
+        organism = 'homo'
+        start_p, end_p, start_n, end_n = 1, 2, 1, 2
+        # start_p, end_p = 1, 2
+        # start_n, end_n = 2, 1
+        strand_position = 3
 
     BED_rows = []
     prot_row = ''
@@ -115,18 +137,27 @@ def gen_protein_track(prot_pep_index, prot_CDS_index, prot_list=[], strand='NC_0
         prot_row = ''
         try:
             CDS_block = prot_CDS_index[protein]  # Extract the block of CDS coordinates
-        except:
+        except:                                  # If the protein code is not in the index exit the loop
             proteins_not_found.append(protein)
             continue
 
-        Strand = CDS_block[0][2]  # The strand is in the third position of the first CDS features record
+        # Strand = CDS_block[0][2]  # The strand is in the third position of the first CDS features record
+        #
+        # if Strand == '+':  # If strand positive
+        #     chromStart = CDS_block[0][0]  # Chromosome Start is in the first position of the first record
+        #     chromEnd = CDS_block[-1][1]  # Chromosome End is in the second position of the last record
+        # else:
+        #     chromStart = CDS_block[-1][1]  # In the negative case the bonduaries coordinates
+        #     chromEnd = CDS_block[0][0]  # inverted their orders in the respective records
+
+        Strand = CDS_block[0][strand_position]  #
 
         if Strand == '+':  # If strand positive
-            chromStart = CDS_block[0][0]  # Chromosome Start is in the first position of the first record
-            chromEnd = CDS_block[-1][1]  # Chromosome End is in the second position of the last record
+            chromStart = CDS_block[0][start_p]  # Chromosome Start is in the first position of the first record
+            chromEnd = CDS_block[-1][end_p]  # Chromosome End is in the second position of the last record
         else:
-            chromStart = CDS_block[-1][1]  # In the negative case the bonduaries coordinates
-            chromEnd = CDS_block[0][0]  # inverted their orders in the respective records
+            chromStart = CDS_block[-1][start_n]  # In the negative case the bonduaries coordinates
+            chromEnd = CDS_block[0][end_n]  # inverted their orders in the respective records
 
         print(prot_CDS_index[protein])
         print(f'{chromStart} - {chromEnd}')
@@ -142,18 +173,49 @@ def gen_protein_track(prot_pep_index, prot_CDS_index, prot_list=[], strand='NC_0
 
         blockSizes_str = ''
         blockStarts_str = ''
+        print('CDSb', CDS_block)
         for CDS in CDS_block:
-            print('CDSb', CDS_block)
+            if organism == 'homo': chr_name = CDS[0] # For human genome the chromosome name is in the first position
+
             if Strand == '+':
-                CDS_start = int(CDS[0])
-                CDS_end = int(CDS[1])
+                CDS_start = CDS[start_p]
+                CDS_end = CDS[end_p]
             else:
-                CDS_start = int(CDS[1])
-                CDS_end = int(CDS[0])
+                CDS_start = CDS[start_n]
+                CDS_end = CDS[end_n]
 
-            blockSizes_str += str(abs(CDS_end - CDS_start)) + ','
-            blockStarts_str += str(abs(int(chromStart) - CDS_start)) + ','
+            blockSizes_str += str(int(abs(CDS_end - CDS_start))) + ','
+            blockStarts_str += str(int(abs(chromStart - CDS_start))) + ','
+        if organism == 'homo':
+            blockStarts_str = blockStarts_str.split(',')
+            blockStarts_str = [x for x in blockStarts_str if x != '']
+            blockStarts_str.reverse()
+            blockStarts_str = ','.join(blockStarts_str)
 
+            blockSizes_str = blockSizes_str.split(',')
+            blockSizes_str = [x for x in blockSizes_str if x != '']
+            blockSizes_str.reverse()
+            blockSizes_str = ','.join(blockSizes_str)
+
+        if type(CDS_start) == float:        # For the genomes manipulated with pd.DataFrame the coordinates are floats NOT str
+            tickStart = str(int(tickStart))
+            tickEnd = str(int(tickEnd))
+            chromStart = str(int(chromStart))
+            chromEnd = str(int(chromEnd))
+        #
+        # print(f"""
+        # chr_name        - {type(chr_name)}\n
+        # chromStart      - {type(chromStart)}\n
+        # chromEnd        - {type(chromEnd)}\n
+        # protein         - {type(protein)}\n
+        # Score           - {type(Score)}\n
+        # Strand          - {type(Strand)}\n
+        # tickStart       - {type(tickStart)}\n
+        # tickEnd         - {type(tickEnd)}\n
+        # itemRGB         - {type(itemRGB)}\n
+        # blockCount      - {type(blockCount)}\n
+        # blockSizes_str  - {type(blockSizes_str)}\n
+        # blockStarts_str - {type(blockStarts_str)}\n""")
         prot_row += (chr_name + '\t' + chromStart + '\t' + chromEnd + '\t' + protein + '\t' + Score + '\t' +
                      Strand + '\t' + tickStart + '\t' + tickEnd + '\t' + itemRGB + '\t' + blockCount + '\t' +
                      blockSizes_str + '\t' + blockStarts_str + '\n')
